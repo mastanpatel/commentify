87cda1e6bf08e4e4b9af6dc9148cf02db6f8de53
mastan patel <m.mastanpatel@gmail.com>
2021-06-08 07:14:59 +0530

    added new class files

---
+++ Commentify.API/Commentify.API/Controllers/ChannelController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commentify.API.Controllers
+{
+    public class ChannelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
